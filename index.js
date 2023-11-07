361cc9bc78c485e7aa07eabe63c0052e99dd8596
Syeda Umme Ayman <aymanshoity@gmail.com>
2023-11-07 19:06:24 +0600

    update Assignment section has been done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cookieParser = require('cookie-parser')
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const app=express()
 const port = process.env.PORT || 5000;
 const dotenv=require('dotenv')
@@ -8,7 +10,11 @@ dotenv.config()
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5174','http://localhost:5173'],
+    credentials: true
+}))
+app.use(cookieParser())
 
 
 
@@ -37,7 +43,26 @@ async function run() {
     // await client.connect();
 
     const assignmentCollection= client.db("Assignment-Avengers").collection("assignments")
+    // Auth related 
+    app.post('/jwt',async(req,res)=>{
+        const user=req.body
+        console.log('loggedIn User',user)
+        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:true,
+            sameSite:'none'
 
+        })
+        res.send({success:true})
+    })
+
+    app.post('/logout',async(req,res)=>{
+        const user=req.body;
+        console.log('logout user',user)
+        res.clearCookie('token',{maxAge:0}).send({success:true})
+
+    })
     
     // create Assignment
     app.post('/assignments',async(req,res)=>{
@@ -52,7 +77,39 @@ async function run() {
         const result=await assignmentCollection.find().toArray()
         res.send(result)
     })
+    app.get('/assignments/:id',async(req,res)=>{
+        const id=req.params.id
+        const query={_id:new ObjectId(id)}
+        const result=await assignmentCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.delete('/assignments/:id',async(req,res)=>{
+        const id=req.params.id
+        console.log('the id u want to delete',id)
+        const query={_id: new ObjectId(id)}
+        const result=await assignmentCollection.deleteOne(query)
+        res.send(result)
+    })
     
+    app.put('/assignments/:id',async(req,res)=>{
+        const id=req.params.id
+        const filter={_id :new ObjectId(id)}
+        const options = { upsert: true };
+        const existingAssignment=req.body
+        const updatedAssignment={
+            $set:{
+                title:existingAssignment.title,
+                image:existingAssignment.image,
+                details:existingAssignment.details,
+                level:existingAssignment.level,
+                marks:existingAssignment.marks,
+                date:existingAssignment.date
+            }
+        }
+        const result=await assignmentCollection.updateOne(filter,updatedAssignment,options)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
